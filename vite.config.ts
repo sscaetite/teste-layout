6dbc23196fb7df8be1a8d2338dfacf46863504d7
Samuel Caetité <samuel.silva@driven.cx>
2023-12-14 23:30:14 -0300

    Teste Layout

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/texte-layout/",
+  base: "/teste-layout/",
   plugins: [react()],
 });
